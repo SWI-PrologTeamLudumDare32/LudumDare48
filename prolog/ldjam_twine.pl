2c036f74ea6984040e91a84a741dd87bd85985a0
Anne Ogborn <annie66us@yahoo.com>
2021-04-27 02:06:58 +0200

    removing xml files from repo - ifdefed out graphml maker - art sources

--- prolog/ldjam_twine.pl
+++ prolog/ldjam_twine.pl
@@ -43,4 +43,48 @@ sandbox:safe_primitive(ldjam_twine:random_root_card(_)).
 
 :- use_module(ifml).
 
-% :- load_ifml_to_cards('xml/testgame.xml').
+
+		 /*******************************
+		 *     GraphML generation
+		 *******************************/
+:- use_module(library(ugraphs)).
+
+% :- pack_install(graphml).
+:- if(false).
+:-use_module(library(graphml_ugraph)).
+
+write_cards_graphml :-
+    setof(Edge, edge(Edge), E),
+    vertices_edges_to_ugraph([], E, UG),
+    vertices(UG, V),
+    setup_call_cleanup(
+        open('map.graphml', write, S),
+        graphml_write_ugraph(S, makename, V, UG),
+        close(S)
+        ).
+
+
+edge(Me-Thee) :-
+    ldjam_twine:card(C),
+    Me = C.name,
+    member(B, C.buttons),
+    Thee = B.go,
+    Thee \= '' .
+
+makename(_, node(Node), Node).
+makename(_, edge(From, To), N) :-
+    format(atom(N), '~w -> ~w', [From, To]).
+:- endif.
+
+
+
+
+
+
+
+
+
+
+
+
+
